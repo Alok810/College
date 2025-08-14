866558e2818571df99a290bb15d817f76c703395
Alok810 <154756272+Alok810@users.noreply.github.com>
2025-08-14 22:42:19 +0530

    new change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Home() {
   return (
     <div>
-      <h2 className="text-2xl font-bold">Home</h2>
+      <h2 className="text-2xl font-bold bg-amber-500">Home</h2>
       <p>Welcome to the College Dashboard!</p>
     </div>
   );
