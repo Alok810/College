7939ba6a141a47b875dd18740b42ef7d4c3b4346
Alok810 <154756272+Alok810@users.noreply.github.com>
2026-05-13 17:42:51 +0530

    pod4

--- src/context/FriendContext.jsx
+++ src/context/FriendContext.jsx
@@ -14,7 +14,7 @@ import {
 const FriendContext = createContext();
 
 const ENDPOINT = import.meta.env.MODE === "production" 
-  ? "https://rigya-backend.onrender.com" 
+  ? "https://aishe.rigya.in" 
   : "http://localhost:4000";
 
 export const FriendProvider = ({ children }) => {
